b4fb51b872953ce5fb52e738af1850596e53b7b0
tracer-pa2017 <tracer@njuics.org>
2017-09-19 11:20:54 +0800

    >  nemu 161220061 lengjiayi Linux lengjiayi 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  11:20:54 up  6:42,  5 users,  load average: 0.33, 0.12, 0.03 d5688a11d7582eff21cbf0bcb45c6acbe4c5724

--- nemu/src/cpu/instr/group.c
+++ nemu/src/cpu/instr/group.c
@@ -9,10 +9,10 @@
 
 make_group_impl(group_1_b);
 make_group_impl(group_1_v);
-make_group_impl(group_1_bv);
+//make_group_impl(group_1_bv);
 make_group_impl(group_2_b);
 make_group_impl(group_2_v);
-//make_group_impl(group_2_1b);
+make_group_impl(group_2_1b);
 make_group_impl(group_2_1v);
 make_group_impl(group_2_cb);
 make_group_impl(group_2_cv);
