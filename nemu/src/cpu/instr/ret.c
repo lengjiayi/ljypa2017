7b5ecc5e77f00585f0c18c752c70fa80dce6be52
tracer-pa2017 <tracer@njuics.org>
2017-09-19 13:59:03 +0800

    >  nemu 161220061 lengjiayi Linux lengjiayi 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  13:59:03 up  9:20,  8 users,  load average: 0.00, 0.00, 0.00 e29d98b02a456eac78bb6068861dbf585bb19cd3

---
+++ nemu/src/cpu/instr/ret.c
@@ -0,0 +1,14 @@
+#include"cpu/instr.h"
+
+make_instr_func(ret_near)
+{
+	OPERAND Mesp;
+	Mesp.type=OPR_MEM;
+	Mesp.addr=cpu.esp;
+	Mesp.data_size=data_size;
+	operand_read(&Mesp);
+	if(data_size==16)
+		Mesp.val&=0x0000ffff;
+	cpu.eip=Mesp.val;
+	return 0;
+}
