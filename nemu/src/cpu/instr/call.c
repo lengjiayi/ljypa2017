af48a06bc5d53dd61680528292c00e811e0ce84c
tracer-pa2017 <tracer@njuics.org>
2017-09-19 14:03:02 +0800

    >  nemu 161220061 lengjiayi Linux lengjiayi 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  14:03:02 up  9:24,  8 users,  load average: 0.00, 0.00, 0.00 8acd0303db4b6dc28d252c97e0bcbeaf8d8ff735

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -1,13 +1,13 @@
 #include"cpu/instr.h"
 make_instr_func(call_Av)
 {
+	push_ebp(eip,0);
 	OPERAND dfun;
 	dfun.type=OPR_IMM;
 	dfun.addr=eip+1;
 	dfun.data_size=data_size;
 	operand_read(&dfun);
 //	printf("%d\n",dfun.val);
-	push_ebp(eip,0);
 	cpu.eip+=dfun.val+data_size/8+1;
 //	printf("%x\n",cpu.eip);
 	return 0;
