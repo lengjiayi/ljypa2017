9a199556e6a5b36e2d497f40cc9cad0f2085df30
tracer-pa2017 <tracer@njuics.org>
2017-09-20 15:41:29 +0800

    >  nemu 161220061 lengjiayi Linux lengjiayi 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  15:41:29 up  7:36,  8 users,  load average: 0.06, 0.12, 0.07 872764b85a89a1b0b0435f28e77454f5f9dbc931

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -68,7 +68,7 @@ make_instr_func(lea)
 	len+=modrm_r_rm(eip+1,&r,&rm);
 //	printf("%d,%d\n",r.addr,rm.addr);
 	rm.val=r.val;
-	operand_write(&r);
+//	operand_write(&r);
 	printf("%d",cpu.edx);
 //	printf("aware me at lea instr/add.c\n");
 	return len;
