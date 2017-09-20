7a4b3c01635efdb7ba511e28a7c2d6f261483923
tracer-pa2017 <tracer@njuics.org>
2017-09-20 15:52:17 +0800

    >  test 161220061 lengjiayi Linux lengjiayi 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  15:52:17 up  7:47,  8 users,  load average: 0.20, 0.20, 0.12 1cf136f2eb874605ab74966076b20b847f08b21d

--- nemu/src/cpu/instr/push.d
+++ nemu/src/cpu/instr/push.d
@@ -8,7 +8,8 @@ src/cpu/instr/push.o: src/cpu/instr/push.c include/cpu/instr.h \
  include/cpu/instr/jmp.h include/cpu/instr/shift.h \
  include/cpu/instr/flags.h include/cpu/instr/group.h \
  include/cpu/instr/special.h include/cpu/instr/x87.h \
- include/cpu/instr/push.h
+ include/cpu/instr/push.h include/cpu/instr/add.h include/cpu/instr/pop.h \
+ include/cpu/instr/inc.h include/cpu/instr/and.h
 
 include/cpu/instr.h:
 
@@ -57,3 +58,11 @@ include/cpu/instr/special.h:
 include/cpu/instr/x87.h:
 
 include/cpu/instr/push.h:
+
+include/cpu/instr/add.h:
+
+include/cpu/instr/pop.h:
+
+include/cpu/instr/inc.h:
+
+include/cpu/instr/and.h:
