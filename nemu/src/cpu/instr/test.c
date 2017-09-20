7185837addea4b1530fbd0d17d48aca24a0c68b8
tracer-pa2017 <tracer@njuics.org>
2017-09-20 09:05:22 +0800

    >  nemu 161220061 lengjiayi Linux lengjiayi 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  09:05:22 up  1:00,  7 users,  load average: 0.04, 0.06, 0.06 1fea40c16b2dfdb54aa7184cd9cb85c4d4a51f9a

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -5,7 +5,7 @@ make_instr_func(test_r2rm_v)
 	int len=1;
 	OPERAND r,rm;
 	r.data_size=rm.data_size=data_size;
-	len+=modrm_r_rm(eip+1,r,rm);
+	len+=modrm_r_rm(eip+1,&r,&rm);
 	operand_read(&r);
 	operand_read(&rm);
 	alu_sub(r.val,rm.val);
