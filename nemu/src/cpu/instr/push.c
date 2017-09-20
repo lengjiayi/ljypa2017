dee84bd8494940a0f2b0b6ce8fac6f97e8b03c8b
tracer-pa2017 <tracer@njuics.org>
2017-09-20 10:04:26 +0800

    >  nemu 161220061 lengjiayi Linux lengjiayi 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  10:04:26 up  1:59,  7 users,  load average: 0.16, 0.10, 0.11 189fdd7a896a1fc222b6a490a0e6ae3b37dca83

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -1,11 +1,19 @@
 #include"cpu/instr.h"
 
-make_instr_func(push_ebp)
+void push(uint32_t &reg,unsigned len)
 {
 	OPERAND ESP;
-	ESP.val=cpu.ebp;
+	ESP.val=reg;
 	ESP.addr=cpu.esp;
 	ESP.data_size=32;
 	operand_write(&ESP);
-	return 1;
+	return len;
+}
+make_instr_func(push_ebp)
+{
+	return push(cpu.ebp,1);
+}
+make_instr_func(push_ebx)
+{
+	return push(cpu.ebx,1);
 }
