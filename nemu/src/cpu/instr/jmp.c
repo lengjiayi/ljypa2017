470c6c50035946ce4a0af4c23275c99c9b32a49c
tracer-pa2017 <tracer@njuics.org>
2017-09-20 10:39:53 +0800

    >  nemu 161220061 lengjiayi Linux lengjiayi 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  10:39:53 up  2:34,  7 users,  load average: 0.00, 0.02, 0.05 593429cee0ee2ccaaa7800a21dfe427900a0be8

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -16,6 +16,24 @@ make_instr_func(jmp_near) {
 
         return 1 + data_size / 8;
 }
+make_instr_func(jmp_short)
+{
+		OPERAND rel;
+        rel.type = OPR_IMM;
+		rel.sreg = SREG_CS;
+        rel.data_size = 8;
+        rel.addr = eip + 1;
+
+        operand_read(&rel);
+
+	int offset = sign_ext(rel.val, data_size);
+	print_asm_1("jmp", "", 2, &rel);
+
+	cpu.eip += offset;
+
+        return 1 + data_size / 8;
+}
+m
 make_instr_func(je)
 {
 //	printf("ZF:%d\n",cpu.eflags.ZF);
