b2ca4b91b1c8573015d3891f7aa09b3608bd935e
tracer-pa2017 <tracer@njuics.org>
2017-09-21 09:04:19 +0800

    >  nemu 161220061 lengjiayi Linux lengjiayi 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  09:04:19 up 12:42,  8 users,  load average: 0.01, 0.02, 0.00 a0108d10b6d3dea2d73d3d3f20df654b841279e

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -67,7 +67,7 @@ int jump(int eip,int dsize)
 }
 make_instr_func(je)
 {
-	printf("je:eax:%d,ecx:%d\n",cpu.eax,cpu.ecx);
+//	printf("je:eax:%d,ecx:%d\n",cpu.eax,cpu.ecx);
 	print_asm_0("je","",10);
 	if(cpu.eflags.ZF==0)
 	{ 
--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -12,6 +12,6 @@ make_instr_func(ret_near)
 	cpu.eip=Mesp.val;
 	cpu.esp+=data_size/8;
 	print_asm_0("ret","",1);
-	printf("ret at:%x, eax:%d,edx:%d\n",cpu.eip,cpu.eax,cpu.edx);
+//	printf("ret at:%x, eax:%d,edx:%d\n",cpu.eip,cpu.eax,cpu.edx);
 	return 0;
 }
