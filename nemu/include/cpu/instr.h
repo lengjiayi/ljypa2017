a37e39bb513ebfd7199571643eb8c96dce6c892f
tracer-pa2017 <tracer@njuics.org>
2017-09-22 03:58:46 +0800

    >  nemu 161220061 lengjiayi Linux lengjiayi 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  03:58:46 up 15:47,  8 users,  load average: 0.00, 0.02, 0.04 d2b8a19efea6f0e9ef8ef4af14324af113ac6bf2

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -28,4 +28,5 @@ extern uint8_t data_size;
 #include "cpu/instr/inc.h"
 #include "cpu/instr/and.h"
 #include "cpu/instr/adc.h"
+#include "cpu/instr/xor.h"
 #endif
---
+++ nemu/src/cpu/instr/xor.c
@@ -0,0 +1,22 @@
+#include "cpu/instr.h"
+
+int xor_r2r(int eip,int dsize)
+{
+	int len=1;
+	OPERAND r,rm;
+	r.data_size=rm.data_size=dsize;
+	len+=modrm_r_rm(eip+1,&r,&rm);
+	operand_read(&r);
+	operand_read(&rm);
+	rm.val=alu_xor(r.val,rm.val);
+	print_asm_2("xor",(dsize==8)?"b":"v",10,&r,&rm);
+	return len;
+}
+make_instr_func(xor_r2r_b)
+{
+	return xor_r2r(eip,8);
+}
+make_instr_func(xor_r2r_v)
+{
+	return xor_r2r(eip,data_size);
+}
