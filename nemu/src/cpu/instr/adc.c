a770228e992d94e001aca69ad0b9e879e12d678d
tracer-pa2017 <tracer@njuics.org>
2017-09-22 02:07:32 +0800

    >  nemu 161220061 lengjiayi Linux lengjiayi 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  02:07:32 up 15:10,  8 users,  load average: 0.07, 0.03, 0.05 d2bde74546701b353ae536ffbee9ddbab5a90cd2

--- nemu/src/cpu/instr/adc.c
+++ nemu/src/cpu/instr/adc.c
@@ -8,7 +8,7 @@ make_instr_func(adc_rm2rm_v)
 	len+=modrm_r_rm(eip+1,&r,&rm);
 	operand_read(&r);
 	operand_read(&rm);
-	rm.val=alu_adc(sign_ext(r.val,data_size),sign_ext(rm.val,data_size);
+	rm.val=alu_adc(sign_ext(r.val,data_size),sign_ext(rm.val,data_size));
 	operand_write(&rm);
 	print_asm_2("adc","v",10,&r,&rm);
 	return len;
