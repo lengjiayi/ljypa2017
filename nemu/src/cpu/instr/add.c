00107e049136c8e4d8baf731d6c1d8d85228271f
tracer-pa2017 <tracer@njuics.org>
2017-09-22 00:16:44 +0800

    >  nemu 161220061 lengjiayi Linux lengjiayi 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  00:16:44 up 13:19,  8 users,  load average: 0.00, 0.05, 0.02 16e03fe76e2f4167fd72dcc9ff4fac063358a439

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -53,7 +53,7 @@ make_instr_func(add_rv2rv)
 	int len=1;
 	OPERAND r,rm;
 	r.data_size=rm.data_size=data_size;
-	len+=modrm_r_rm(eip,&r,&rm);
+	len+=modrm_r_rm(eip+1,&r,&rm);
 	operand_read(&r);
 	printf("In add rv\n");
 	operand_read(&rm);
