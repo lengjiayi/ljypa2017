762b393b8d6f6c6470a4aa48555ae78b963a984c
tracer-pa2017 <tracer@njuics.org>
2017-09-22 03:59:30 +0800

    >  nemu 161220061 lengjiayi Linux lengjiayi 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  03:59:30 up 15:47,  8 users,  load average: 0.16, 0.05, 0.04 8f36aec0a454557918cc423036b2c12aa05aacc5

--- nemu/include/cpu/instr/xor.h
+++ nemu/include/cpu/instr/xor.h
@@ -1,14 +1,7 @@
-#ifndef __INSTR_POP_H__
-#define __INSTR_POP_H__
+#ifndef __INSTR_XOR_H__
+#define __INSTR_XOR_H__
 
-make_instr_func(pop_mv);
-make_instr_func(pop_eax);
-make_instr_func(pop_ecx);
-make_instr_func(pop_edx);
-make_instr_func(pop_ebx);
-make_instr_func(pop_esp);
-make_instr_func(pop_ebp);
-make_instr_func(pop_esi);
-make_instr_func(pop_edi);
+make_instr_func(xor_r2r_b);
+make_instr_func(xor_r2r_v);
 
 #endif
