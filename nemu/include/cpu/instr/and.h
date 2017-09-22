fc5ac172d883ff2adc35e64e0d28b252163859e4
tracer-pa2017 <tracer@njuics.org>
2017-09-22 09:11:33 +0800

    >  nemu 161220061 lengjiayi Linux lengjiayi 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  09:11:33 up 18:27,  8 users,  load average: 0.07, 0.06, 0.07 4b0e54a8ffe44a4ac9f0e8161c6f743ebeb41dd8

--- nemu/include/cpu/instr/and.h
+++ nemu/include/cpu/instr/and.h
@@ -4,4 +4,7 @@
 make_instr_func(and_r2r_b);
 make_instr_func(and_si2r_v);
 make_instr_func(and_r2r_v);
+make_instr_func(and_r2r_b_r);
+make_instr_func(and_r2r_v_r);
+
 #endif
--- nemu/src/cpu/instr/and.c
+++ nemu/src/cpu/instr/and.c
@@ -1,5 +1,5 @@
 #include "cpu/instr.h"
-int and_r2r(int eip,int dsize)
+int and_r2r(int eip,int dsize,bool reverse)
 {
 	int len=1;
 	OPERAND r,rm;
@@ -8,20 +8,39 @@ int and_r2r(int eip,int dsize)
 	operand_read(&r);
 	operand_read(&rm);
 //	printf("%x,%x\n",r.val,rm.val);
-	rm.val=alu_and(r.val,rm.val);
-//	printf("%x\n",rm.val);
-	operand_write(&rm);
-	print_asm_2("and",(dsize==8)?"b":"v",10,&r,&rm);
+	if(reverse)
+	{	
+		r.val=alu_and(r.val,rm.val);
+//		printf("%x\n",rm.val);
+		operand_write(&r);
+		print_asm_2("and",(dsize==8)?"b":"v",10,&rm,&r);
+	}
+	else
+	{
+		rm.val=alu_and(r.val,rm.val);
+//		printf("%x\n",rm.val);
+		operand_write(&rm);
+		print_asm_2("and",(dsize==8)?"b":"v",10,&r,&rm);
+	}
 	return len;	
 }
 make_instr_func(and_r2r_v)
 {
-	return and_r2r(eip,data_size);
+	return and_r2r(eip,data_size,0);
 }
 make_instr_func(and_r2r_b)
 {
-	return and_r2r(eip,8);
+	return and_r2r(eip,8,0);
 }
+make_instr_func(and_r2r_v_r)
+{
+	return and_r2r(eip,data_size,1);
+}
+make_instr_func(and_r2r_b_r)
+{
+	return and_r2r(eip,8,1);
+}
+
 make_instr_func(and_si2r_v)
 {
 	int len=1;
